47f6a7ec3c88bbc9ea169b6428545d240cdfc7bf
mbonilla814 <miguelbonilla814+2@gmail.com>
2022-02-23 21:49:06 -0500

    toolbar y footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { FooterComponent } from './components/footer/footer.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'footer', component: FooterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
