3ac3cf9c378d2ae4d82650ba2b76fc7479dee242
mbonilla814 <miguelbonilla814+2@gmail.com>
2022-02-24 15:33:44 -0500

    inicio final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,14 +12,10 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
   ],
   imports: [
     BrowserModule,
